fd80073229b1a50b7d952f7ab781ee4e2b648818
Olga <kapitalinaivanovna@gmail.com>
2016-11-25 07:32:27 +0200

    few functions added

--- sandbox/src/main/java/ua/javatests/MyFirstProgram.java
+++ sandbox/src/main/java/ua/javatests/MyFirstProgram.java
@@ -6,5 +6,26 @@ public class MyFirstProgram {
   
   System.out.println("Hello, world!!");
 
+      hello("user");
+      hello("friend");
+
+      double l = 111.1;
+      System.out.println("square with side length "+l+" equals "+area(l));
+      double a = 2;
+      double b = 3;
+      System.out.println("square with side length "+a+"and "+b+ " equals "+area(a,b));
  }
+
+    private static void hello(String user) {
+        System.out.println("Hello, "+user);
+    }
+
+    public static double area(double length){
+        return length* length;
+    }
+
+    public static double area(double l1, double l2){
+        return l1*l2;
+    }
+
 }
