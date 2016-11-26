961ed5932a3f2fe7f8a148d6bcd9a19529510134
Olga <kapitalinaivanovna@gmail.com>
2016-11-26 08:26:33 +0200

    Point class pus test method created

---
+++ sandbox/src/main/java/ua/javatests/Point.java
@@ -0,0 +1,18 @@
+package ua.javatests;
+
+public class Point {
+    public double x;
+    public double y;
+
+    public Point(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+    public static double distance(Point p1, Point p2){
+        return Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
+    }
+
+    public double distanceToPoint(Point p2){
+        return Math.sqrt(Math.pow((p2.x - this.x), 2) + Math.pow((p2.y - this.y), 2));
+    }
+}
